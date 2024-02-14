2f1d29b4f97b6a4e93a9c5f2f92a15625f77caa2
AhmedZeyad1 <439102481@student.ksu.edu.sa>
2024-02-14 20:57:05 +0300

    q1 finished..

--- Lab3.py
+++ Lab3.py
@@ -1,27 +1,34 @@
 # IS424
 # Lab (3)
+
 # { Ahmed Alahmed | 439 102 481 }
 
-
-# 1st. Program:
 def emp_sal_program():
+    print("Hello User, Welcome to the emp_sal_program..")
     try:
-        print("Hello User,, Welcome to the emp_sal_program..")
+        programEngine = input("Please enter (s) to start the program or (e) to exit program: ")
         
-        checkValue = input("Please entre any words to start the program or (no) to stop.")
+        if programEngine.lower() == "s":
+            empDic = {}
+            checkValue = input("Enter [ (go) or any words to start adding] or (no) to stop: ")
 
-        while checkValue is not ("no" or "No" or "NO"):
-            empKey = input("Enter employee name: ")
-            empDic = {} == empKey
+            while checkValue.lower() != "no":
+                empKey = input("Enter employee name: ")
+                emp_keyValue = int(input("Enter employee salary: "))
+                empDic[empKey] = emp_keyValue
+                checkValue = input("Enter [ (a) or any words to continue adding] or (no) to stop: ")
+            print("The dictionary of emp_salary_program is: ", empDic)
+            print("Have a nice day user..")
 
-            emp_keyValue = input(int)("Enter employee salary: ")
-            empDic[empKey] == emp_keyValue
-        print("The dictionory of emp_salary_program is: "+empDic)
-        print("Have a nice day user")    
-    except Exception (e):
-        print("The problem is "+ e +" Or something went wrong, please try again..")
-        emp_sal_scanner()
-
+        elif programEngine.lower() == "e":
+            print("You have exited the program, have a nice day user..")
+        else:
+            print("User, try again with the same input..")
+            emp_sal_program()
+            
+    except Exception as e:
+        print("The problem is ",e," Or something went wrong, please try again..")
+        emp_sal_program()
 
 # Testing P1:
-emp_sal_program()        
+emp_sal_program()
